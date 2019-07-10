dcd9fd31a44983c02a7c83f18227f07ceecb9fd8
Saman Behrouzi <samanbehrouzi@gmail.com>
2019-07-10 18:47:14 +0430

    commented prediction

--- apis/views.py
+++ apis/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from apis.models import Car
 import json
-from prediction import perform_prediction
+# from prediction import perform_prediction
 
 def index(request):
     response = json.dumps([{}])
@@ -32,35 +32,35 @@ def add_car(request):
             response = json.dumps([{'Error': 'Car could not be added!'}])
     return HttpResponse(response, content_type='text/json')
 
-@csrf_exempt
-def add_car(request):
-    if request.method == 'POST':
-        payload = json.loads(request.body)
-        lines = payload['lines']
-
-        from joblib import load
-        import pandas as pd
-        import json
-        # testdf = pd.DataFrame([[1,2],[3,4]], columns=["a", "b"])
-        # testdf.to_csv("D:\\vhosts\\cbb1.ut.ac.ir\\httpdocs\\UploadedFiles\\testdf.csv", sep=",", index=False)
-
-        # lines = json.loads(test_file_lines)
-        df = pd.DataFrame(columns = list(lines[0].split(",")))
-        for line, ind in zip(lines[1:], range(len(lines)-1)):
-            df.loc[ind] = line
-
-        model = load('SVMModel.joblib') # 'SVMModel.joblib'
-        # X_test = pd.read_csv(test_file_path)
-        X_test = df
-        predictions = model.predict(X_test.iloc[:,1:])
-        df = pd.DataFrame(columns=["Peptide sequence", "Biofilm inhobitor"])
-        seqs = list(X_test.seq)
-        for indx in range(len(predictions)):
-            df = df.append({"Peptide sequence": seqs[indx], "Biofilm inhobitor": str(predictions[indx]).replace("0", "non BIP").replace("1", "BIP")}, ignore_index=True)
-        df.to_csv(output_file_path, sep=',', index=False)
-        # print(X_test)
-        # print(predictions)
-        # print(len(predictions))
+# @csrf_exempt
+# def add_car(request):
+#     if request.method == 'POST':
+#         payload = json.loads(request.body)
+#         lines = payload['lines']
+#
+#         from joblib import load
+#         import pandas as pd
+#         import json
+#         # testdf = pd.DataFrame([[1,2],[3,4]], columns=["a", "b"])
+#         # testdf.to_csv("D:\\vhosts\\cbb1.ut.ac.ir\\httpdocs\\UploadedFiles\\testdf.csv", sep=",", index=False)
+#
+#         # lines = json.loads(test_file_lines)
+#         df = pd.DataFrame(columns = list(lines[0].split(",")))
+#         for line, ind in zip(lines[1:], range(len(lines)-1)):
+#             df.loc[ind] = line
+#
+#         model = load('SVMModel.joblib') # 'SVMModel.joblib'
+#         # X_test = pd.read_csv(test_file_path)
+#         X_test = df
+#         predictions = model.predict(X_test.iloc[:,1:])
+#         df = pd.DataFrame(columns=["Peptide sequence", "Biofilm inhobitor"])
+#         seqs = list(X_test.seq)
+#         for indx in range(len(predictions)):
+#             df = df.append({"Peptide sequence": seqs[indx], "Biofilm inhobitor": str(predictions[indx]).replace("0", "non BIP").replace("1", "BIP")}, ignore_index=True)
+#         df.to_csv(output_file_path, sep=',', index=False)
+#         # print(X_test)
+#         # print(predictions)
+#         # print(len(predictions))
 
 
 
